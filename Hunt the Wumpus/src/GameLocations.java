e34c86fb58ebf27d272449a3fb0d0f110ce50c5c
Shrey Srivastava <s-ssrivastava@lwsd.org>
2019-03-14 17:47:02 +0000

    GameLocations.java edited online with Bitbucket

--- Hunt the Wumpus/src/GameLocations.java
+++ Hunt the Wumpus/src/GameLocations.java
@@ -1,4 +1,25 @@
 
 public class GameLocations {
-
+public GameLocations() {
+	
+}
+// returns the name of the object
+public String toString() {
+	return "GameLocations";
+}
+// returns an array of where the hazards are, needs cave for map
+public int[] trackHazards(int[] map) {
+	int[] hazards = {0};
+	return hazards;
+}
+// returns the integer position of the wumpus, needs cave for map
+public int trackWumpus(int[] map) {
+	int pos = 0;
+	return pos;
+}
+//returns the integer position of the player, needs cave for map
+public int Player(int[] map) {
+	int pos = 0;
+	return pos;
+}
 }
