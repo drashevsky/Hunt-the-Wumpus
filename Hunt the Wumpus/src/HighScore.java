31da255fb96cafb17d4b0929bc42cb18aac4c86e
s-DRASHEVSKY <s-DRASHEVSKY@487M065096.lwsd.k12wa.org>
2019-03-14 12:05:33 -0700

    Trying to add in stuff...

--- Hunt the Wumpus/src/HighScore.java
+++ Hunt the Wumpus/src/HighScore.java
@@ -1,4 +1,32 @@
-
+//Deepayan Sanyal pd. 4 Group Big Brother
+//Object HighScore, keeps track of player's high score
+	// 3.5.18 - First Assignment: Class Header + Constructor + toString
 public class HighScore {
 
+	private int[] highScores;
+	
+	public HighScore() {
+		
+	}
+	
+	//Returns the top 10 High Scores as an array of integers
+	private int[] getHighScores() {
+		return new int[10];
+	}
+	
+	//Returns the High Score at the given index. The integer paramter represents the index of the High Score.
+	private int getHighScore(int index) {
+		return 0;
+	}
+	
+	//Adds the score given as an integer parameter to the highScores array at the appropriate positions.
+	//Returns an boolean representing whether the score is a High Score
+	private boolean addScore(int score) {
+		return false;
+	}
+	
+	public String toString() {
+		return "HighScore";
+	}
+	
 }
