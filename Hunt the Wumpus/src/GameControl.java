5a7a316eea2177e11949bafa3ef1f2521ac722e4
s-EANDERSON1 <s-EANDERSON1@487M053613.lwsd.k12wa.org>
2019-03-14 11:03:13 -0700

    Added my code woo

--- Hunt the Wumpus/src/GameControl.java
+++ Hunt the Wumpus/src/GameControl.java
@@ -1,4 +1,43 @@
-
+/**
+ * 
+ * @author Eric Anderson
+ * Date: 3/13/19
+ * Version 1.0
+ * Comments: 
+ * 	V1.0
+ * 		- Added main method
+ * 		- Created instantiations of each object
+ */
 public class GameControl {
-
+	public static void main(String[] args) {
+		// Create an instance of each Object
+		Cave cave = new Cave();
+		GameLocations gameLocations = new GameLocations();
+		GraphicalInterface gui = new GraphicalInterface();
+		HighScore highscore = new HighScore();
+		Player player = new Player();
+		Trivia trivia = new Trivia();
+	}
+	
+	// Determines the keystrokes of the player and validates the input
+	// Returns false if invalid
+	public static boolean userInput(Player player) {
+		return true;
+	}
+	
+	// Determines the game location
+	public static boolean gameState(GameLocations gameLocations) {
+		return true;
+	}
+	
+	// Manages the value of the high score
+	public static boolean highScoreManagement(HighScore highScore) {
+		return true;
+	}
+	
+	// Logic updater
+	// (May change this later if there is another way)
+	public static boolean update(float dt) {
+		return true;
+	}
 }
