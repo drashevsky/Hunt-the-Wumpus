b9b03b42a2c70477958f9c775f2bb63b85e23ac4
s-SSRIVASTAVA <s-SSRIVASTAVA@487M078506.lwsd.k12wa.org>
2019-03-21 10:51:58 -0700

    Hi

--- Hunt the Wumpus/src/Player.java
+++ Hunt the Wumpus/src/Player.java
@@ -13,25 +13,13 @@ public String toString()
 {
 	return "Player";
 }
-// Moves the player based on its current room, needs location from Cave
-public int[] Move(int currentRoom) {
-	int[] connectedRooms = {0, 0, 0, 0, 0, 0};
-	return connectedRooms;
-}
-// Checks if the shot arrow hit the Wumpus, supposed to be used by game control
-public boolean shootArrow(int Wumpus, int Arrow)
-{
-	if (Wumpus == Arrow) {
-		return true;
-	}
-	return false;
-}
-// Purchases arrow based on the number of questions answered, will probably call Trivia method for parameter
+//Purchases arrow based on the number of questions answered, will probably call Trivia method for parameter
 public boolean purchaseArrows(int questionsAnswered)
 {
 	if (questionsAnswered > 2)
 	{
 		questionsAnswered -=2;
+		this.arrows += 1;
 		return true;
 	}
 	return false;
@@ -42,6 +30,7 @@ public boolean purchaseSecrets(int questionsAnswered)
 	if (questionsAnswered > 2)
 	{
 		questionsAnswered -=2;
+		this.secrets += 1;
 		return true;
 	}
 	return false;
