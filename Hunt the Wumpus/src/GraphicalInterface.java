f57b659ce2fd814302507676316219954b0a1f82
Vinayak Raj Ajmera <s-vajmera@lwsd.org>
2019-03-21 15:14:30 +0000

    GraphicalInterface.java edited online with Bitbucket

--- Hunt the Wumpus/src/GraphicalInterface.java
+++ Hunt the Wumpus/src/GraphicalInterface.java
@@ -24,7 +24,7 @@ public class GraphicalInterface {
 	//draw cave- taking a param of the starting room
 	public void drawCave(int room)
 	{
-		System.out.print(room);
+		System.out.println(room);
 	}
 	
 	//display the Player score (param - score)
