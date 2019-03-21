b39c3193a95cb6d17eb3c1940f16b0800fec67dc
s-VAJMERA <s-VAJMERA@487M078457.lwsd.k12wa.org>
2019-03-21 13:17:43 -0700

    fixed bug

--- Hunt the Wumpus/src/GraphicalInterface.java
+++ Hunt the Wumpus/src/GraphicalInterface.java
@@ -1,5 +1,5 @@
 
-public class GraphicalInterface {
+public class GraphicalInterface{
 	/* Name: V. Raj Ajmera
 	 * Class Name: Graphical Interface
 	 * Object: Displaying state of the current game
@@ -11,7 +11,16 @@ public class GraphicalInterface {
 	
 	//constructor
 
+	import java.awt.Dimension;
+	import java.awt.EventQueue;
+	import java.awt.Graphics;
+	import java.awt.Graphics2D;
+	import java.awt.Panel;
 	import javax.swing.JFrame;
+	import javax.swing.JPanel;
+	import javax.swing.UIManager;
+	import javax.swing.UnsupportedLookAndFeelException;
+
 	public GraphicalInterface()
 	{
 		
@@ -57,52 +66,5 @@ public class GraphicalInterface {
 		System.out.print(actions);
 	}
 	
-	public static void drawCanvas()
-	{
-
-	    final String title = "Test Window";
-	    final int width = 1200;
-	    final int height = width / 16 * 9;
-
-	    //Creating the frame.
-	    JFrame frame = new JFrame(title);
-
-	    frame.setSize(width, height);
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setLocationRelativeTo(null);
-	    frame.setResizable(false);
-	    frame.setVisible(true);
-
-	    //Creating the canvas.
-	    Canvas canvas = new Canvas();
-
-	    canvas.setSize(width, height);
-	    canvas.setBackground(Color.BLACK);
-	    canvas.setVisible(true);
-	    canvas.setFocusable(false);
-
-
-	    //Putting it all together.
-	    frame.add(canvas);
-
-	    canvas.createBufferStrategy(3);
-
-	    boolean running = true;
-
-	    BufferStrategy bufferStrategy;
-	    Graphics graphics;
-
-	    while (running) {
-	        bufferStrategy = canvas.getBufferStrategy();
-	        graphics = bufferStrategy.getDrawGraphics();
-	        graphics.clearRect(0, 0, width, height);
-
-	        graphics.setColor(Color.GREEN);
-	        graphics.drawString("This is some text placed in the top left corner.", 5, 15);
-
-	        bufferStrategy.show();
-	        graphics.dispose();
-	    }
-	}
 
 }
