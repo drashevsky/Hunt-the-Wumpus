804e4cdd91edf314f49973871cc7f5b1f1ac8ffa
s-VAJMERA <s-VAJMERA@487M078457.lwsd.k12wa.org>
2019-03-27 10:03:57 -0700

    window

--- Hunt the Wumpus/src/GraphicalInterface.java
+++ Hunt the Wumpus/src/GraphicalInterface.java
@@ -1,5 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GraphicalInterface{
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+public class GraphicalInterface implements ActionListener {
 	/* Name: V. Raj Ajmera
 	 * Class Name: Graphical Interface
 	 * Object: Displaying state of the current game
@@ -11,19 +17,61 @@ public class GraphicalInterface{
 	
 	//constructor
 
-	import java.awt.Dimension;
-	import java.awt.EventQueue;
-	import java.awt.Graphics;
-	import java.awt.Graphics2D;
-	import java.awt.Panel;
-	import javax.swing.JFrame;
-	import javax.swing.JPanel;
-	import javax.swing.UIManager;
-	import javax.swing.UnsupportedLookAndFeelException;
+
 
 	public GraphicalInterface()
 	{
-		
+		JFrame f = new JFrame("Menu Demo");
+	    f.setSize(220, 200);
+
+	    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    JMenuBar jmb = new JMenuBar();
+
+	    JMenu jmFile = new JMenu("File");
+	    JMenuItem jmiOpen = new JMenuItem("Open");
+	    JMenuItem jmiClose = new JMenuItem("Close");
+	    JMenuItem jmiSave = new JMenuItem("Save");
+	    JMenuItem jmiExit = new JMenuItem("Exit");
+	    jmFile.add(jmiOpen);
+	    jmFile.add(jmiClose);
+	    jmFile.add(jmiSave);
+	    jmFile.addSeparator();
+	    jmFile.add(jmiExit);
+	    jmb.add(jmFile);
+
+	    JMenu jmOptions = new JMenu("Options");
+	    JMenu a = new JMenu("A");
+	    JMenuItem b = new JMenuItem("B");
+	    JMenuItem c = new JMenuItem("C");
+	    JMenuItem d = new JMenuItem("D");
+	    a.add(b);
+	    a.add(c);
+	    a.add(d);
+	    jmOptions.add(a);
+
+	    JMenu e = new JMenu("E");
+	    e.add(new JMenuItem("F"));
+	    e.add(new JMenuItem("G"));
+	    jmOptions.add(e);
+
+	    jmb.add(jmOptions);
+
+	    JMenu jmHelp = new JMenu("Help");
+	    JMenuItem jmiAbout = new JMenuItem("About");
+	    jmHelp.add(jmiAbout);
+	    jmb.add(jmHelp);
+
+	    jmiOpen.addActionListener(this);
+	    jmiClose.addActionListener(this);
+	    jmiSave.addActionListener(this);
+	    jmiExit.addActionListener(this);
+	    b.addActionListener(this);
+	    c.addActionListener(this);
+	    d.addActionListener(this);
+	    jmiAbout.addActionListener(this);
+
+	    f.setJMenuBar(jmb);
+	    f.setVisible(true);
 	}
 	
 	//toString
@@ -31,20 +79,14 @@ public class GraphicalInterface{
 	{
 		return "Graphical Interface";
 	}
-	
-	//draw cave- taking a param of the starting room
-	public void drawCave(int room)
-	{
-		System.out.print(room);
-	}
-	
-	//display the Player score (param - score)
+
+	//display the Player score (Parameter - score)
 	public void displayScore(int score)
 	{
 		System.out.print(score);
 	}
 	
-	//display the Player inventory (param - items in an array)
+	//display the Player inventory (parameter - items in an array)
 	public void displayInventory(String[] inventory)
 	{
 		for(String s: inventory)
@@ -60,11 +102,18 @@ public class GraphicalInterface{
 	}
 	
 	//display the actions a Player can take on their turn
-	//takes possible actions as a String param
+	//takes possible actions as a String parameter
 	public void displayOptions(String actions)
 	{
 		System.out.print(actions);
 	}
+	 public void actionPerformed(ActionEvent ae) {
+		    String comStr = ae.getActionCommand();
+		    System.out.println(comStr + " Selected");
+		  }
+		  public static void main(String args[]) {
+		    new GraphicalInterface();
+		  }
 	
 
 }
