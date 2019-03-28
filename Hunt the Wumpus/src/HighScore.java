17c7c5271a27dad694c784424015b72ab6f6ed7e
Deepayan Sanyal <dsanyal@live.com>
2019-03-28 06:28:35 -0800

    Improved High Score
    Retrieves data from text file

--- Hunt the Wumpus/src/HighScore.java
+++ Hunt the Wumpus/src/HighScore.java
@@ -1,22 +1,59 @@
+import java.util.*;
+import java.io.*;
 //Deepayan Sanyal pd. 4 Group Big Brother
 //Object HighScore, keeps track of player's high score
 	// 3.5.18 - First Assignment: Class Header + Constructor + toString
 public class HighScore {
 
 	private int[] highScores;
-	
+	private File highScoresFile;	
+
 	public HighScore() {
+
+		highScoresFile = new File("HighScoresFile.txt);
+
+		retrieveHighScores;
 		
 	}
 	
-	//Returns the top 10 High Scores as an array of integers
-	private int[] getHighScores() {
-		return new int[10];
+	//Returns the top 10 High Scores as an array of integers and retrieves it from the text file
+	private int[] retrieveHighScores() {
+		int counter = 0;
+
+		Scanner textCounter = new Scanner(highScoresFile);
+
+		while(textCounter.hasNextLine()){
+
+			counter++;
+			textCounter.nextLine();
+
+		}
+
+		textCounter.close();
+		
+		Scanner textReader = new Scanner(highScoresFile);
+		highScores = new int[counter];
+
+		for(int i = 0; i < counter; i++){
+
+			highScores[i] = textReader.nextInt();		
+
+		}
+
+		textReader.close();
+
+		return highScores;
+
+	}
+
+	//Returns High Scores as an array
+	public int[] getHighScores(){
+		return highScores;
 	}
 	
 	//Returns the High Score at the given index. The integer paramter represents the index of the High Score.
 	private int getHighScore(int index) {
-		return 0;
+		highScores[i];
 	}
 	
 	//Adds the score given as an integer parameter to the highScores array at the appropriate positions.
