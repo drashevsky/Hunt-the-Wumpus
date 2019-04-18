3d1a1c49fb9b1b052610f637d9d2f2c6a62a8642
Daniel <Daniel@Daniel-PC>
2019-04-17 22:40:08 -0700

    Added a hazard property to room

--- Hunt the Wumpus/src/Room.java
+++ Hunt the Wumpus/src/Room.java
@@ -6,12 +6,14 @@
  * 
  * 3/26/2019 - Added functionality to allow each room to point to three adjacent rooms
  * 3/27/2019 - Testing bitbucket -> discord functionality (just this line)
+ * 4/17/2019 - Added a hazard property
  */
 
 //NOTE: For connected rooms, the int can be either 0 or 1 - 30: if 0, that pathway isn't used 
 
 public class Room {
 	private int[] connectedRooms = new int[3];	//Allow a room to point to a maximum of 3 others, use 0 to indicate an unused pointer
+	private int hazardType = 0; 				//Determine what hazard a room has, 0 = none, 1 = pit, 2 = bat
 	
 	public Room(int r1, int r2, int r3) {
 		connectedRooms[0] = r1;
@@ -19,8 +21,13 @@ public class Room {
 		connectedRooms[2] = r3;
 	}
 	
-	/** Returns an array containing connected rooms to a room */
+	//Returns an array containing connected rooms to a room
 	public int[] getConnectedRooms() {
 		return connectedRooms;
+	}
+	
+	//Returns the type of hazard the room has, or 0 if none
+	public int getHazard() {
+		return hazardType;
 	}
 }
