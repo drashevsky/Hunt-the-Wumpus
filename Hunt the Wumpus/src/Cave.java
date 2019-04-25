813363a7a7b23943f41e027d05ba5b39a3ea26c2
Daniel <Daniel@Daniel-PC>
2019-04-24 23:28:30 -0700

    Updated getters and setters for hazards. Added private mapRows and mapCols facilities to make adjacentRooms compatible with the randomMap generator, developed 2 randomMap generator algorithms and helper methods (both still in testing), fixed loadmap to be compatible with mapRows/Cols. Generated a second map file using randomMap() ;-)

--- Hunt the Wumpus/src/Cave.java
+++ Hunt the Wumpus/src/Cave.java
@@ -14,6 +14,11 @@
  *             Reorganized class, determined public/private/static
  * 4/02/2019 - Wrote some new tests, updated documentation/some methods
  * 4/12/2019 - Updated documentation, removed bulky javadoc
+ * 4/24/2019 - Added private mapRows and mapCols facilities to make 
+ * 			   adjacentRooms compatible with the randomMap generator, 
+ * 			   developed 2 randomMap generator algorithms and helper methods 
+ * 			   (both still in testing), fixed loadmap to be compatible with 
+ *             mapRows/Cols
  */
 
 import java.util.*;
@@ -22,8 +27,12 @@ import java.io.*;
 public class Cave {
 	private Room[][] caveMap;
 	private String mapFile;
+	private int mapRows, mapCols;
 	
 	public Cave(String mapFile) {
+		this.mapRows = 5;
+		this.mapCols = 6;
+		
 		this.mapFile = mapFile;
 		loadMap(mapFile);
 		tester();
@@ -68,22 +77,119 @@ public class Cave {
 				src.nextLine();																//Force next line
 		}
 		
+		this.mapRows = caveMap.length;												//Updates map rows and cols for adjacentRooms
+		this.mapCols = caveMap[0].length;
 		src.close();
 	}
 	
+	//Creates a random map of connected room starting with startRoom (1 - 30)
+	private int[][] randomMap(int startRoom, int numRooms) {
+ 		int[][] map = new int[numRooms][3];
+ 		int currRoom = startRoom;
+ 		int nextRoom;
+ 		
+ 		ArrayList<Integer> stack = new ArrayList<Integer>();
+ 		ArrayList<Integer> usedRooms = new ArrayList<Integer>();
+ 		
+ 		while (unconnectedRoomsExist(map)) {
+ 			int[] adjRooms = this.adjacentRooms(currRoom);
+ 			boolean unconnectedAdjacentRooms = false;
+ 			
+ 			for (int i = 0; i < adjRooms.length; i++) {
+ 	 			for (int j = 0; j < usedRooms.size(); j++) {
+ 	 				if (adjRooms[i] == usedRooms.get(j))
+ 	 					adjRooms[i] = 0;
+ 	 			}
+ 	 			if (adjRooms[i] != 0)
+ 					unconnectedAdjacentRooms = true;
+ 	 		}
+ 			
+ 			if (unconnectedAdjacentRooms) {
+ 				while ((nextRoom = adjRooms[(int)(Math.random() * adjRooms.length)]) == 0);
+ 				stack.add(nextRoom);
+ 				
+ 				int cStartIndex, nStartIndex;
+	 			for (cStartIndex = 0; cStartIndex < 3 && map[currRoom - 1][cStartIndex] != 0; cStartIndex++);
+	 			for (nStartIndex = 0; nStartIndex < 3 && map[nextRoom - 1][nStartIndex] != 0; nStartIndex++);
+	 			
+ 				map[currRoom - 1][cStartIndex] = nextRoom;
+ 				map[nextRoom - 1][nStartIndex] = currRoom;
+ 				currRoom = nextRoom;
+ 				usedRooms.add(currRoom);
+ 			} else if (stack.size() > 0) {
+ 				currRoom = stack.remove(stack.size() - 1);
+ 			}
+ 		}
+ 		return map;
+ 	}
+ 	
+ 	private int[][] randomMap2(int startRoom, int numRooms) {
+ 		int[][] map = new int[numRooms][3];
+
+ 		for (int i = 1; i <= numRooms; i++) {
+ 			int[] adjRooms = this.adjacentRooms(i);
+ 			
+ 			for (int j = 0; j < adjRooms.length; j++) {
+ 	 			if (map[adjRooms[j] - 1][0] != 0 && map[adjRooms[j] - 1][1] != 0 && map[adjRooms[j] - 1][2] != 0)
+ 	 					adjRooms[j] = 0;
+ 	 		}
+ 			
+ 			int count = 0;
+ 			for (int j = 0; j < adjRooms.length; j++) count += adjRooms[j];
+ 			System.out.println(count);
+ 			
+ 			if (map[i - 1][0] == 0 || map[i - 1][1] == 0 || map[i - 1][2] == 0) {
+	 			int startIndex;
+	 			int endIndex = 2;
+	 			for (startIndex = 0; startIndex < 3 && map[i - 1][startIndex] != 0; startIndex++);
+	 			if (startIndex == 0) endIndex = 1;
+	 			
+	 			for (int j = startIndex; j <= endIndex; j++) {
+	 				int randRoom = 0;
+	 				int randRoomIndex = 0;
+	 				
+	 				while (randRoom == 0) {
+	 					randRoomIndex = (int)(Math.random() * adjRooms.length);
+	 					randRoom = adjRooms[randRoomIndex];
+	 				}
+	 				map[i - 1][j] = randRoom;
+	 				
+	 				int startIndexRand;
+		 			for (startIndexRand = 0; startIndexRand < 3 && map[randRoom - 1][startIndexRand] != 0; startIndexRand++);
+	 				map[randRoom - 1][startIndexRand] = i;
+	 				adjRooms[randRoomIndex] = 0;
+	 			}
+ 			}
+ 		}
+ 		return map;
+ 	}
+	
+ 	private boolean unconnectedRoomsExist(int[][] map) {
+ 		for (int i = 0; i < map.length; i++) {
+ 			int a = 0;
+ 			for (int j = 0; j < map[0].length; j++) {
+ 				a += map[i][j];
+ 			}
+ 			if (a == 0) return true;
+ 		}
+ 		return false;
+ 	}
+ 	
+	//Tests all the other methods
+	
  	//Tests all the other methods
-	private void tester() {
+ 	private void tester() {
 		System.out.println("Testing 1D -> 2D index conversion:");
 		for (int i = 1; i <= 30; i++) {
 			System.out.print("[ " + roomRow(i) + ", " + roomCol(i) + " ]" );
-			if (i % caveMap[0].length == 0) {
+			if (i % this.mapCols == 0) {
 				System.out.println();
 			}
 		}
 		
 		System.out.println("\nTesting 2D -> 1D index conversion:");
-		for (int i = 0; i < caveMap.length; i++) {
-			for (int j = 0; j < caveMap[0].length; j++) {
+		for (int i = 0; i < this.mapRows; i++) {
+			for (int j = 0; j < this.mapCols; j++) {
 				System.out.print("[");
 				if (vectorIndexToRoom(i, j) < 10)
 					System.out.print(" ");
@@ -93,11 +199,11 @@ public class Cave {
 		}
 		
 		System.out.println("\nTesting adjacent rooms:");
-		for (int i = 0; i < 6; i++) {
-			for (int j = 1; j <= 30; j++) {
-				if (adjacentRooms(j)[i] < 10)
+		for (int i = 1; i <= 30; i++) {
+			for (int j = 0; j < 6; j++) {
+				if (adjacentRooms(i)[j] < 10)
 					System.out.print(" ");
-				System.out.print(adjacentRooms(j)[i] + " ");
+				System.out.print(adjacentRooms(i)[j] + " ");
 			}
 			System.out.println();
 		}
@@ -111,12 +217,26 @@ public class Cave {
 		
 		System.out.println("\nFile read test: " + caveMap.length + " " + caveMap[0].length);
 		System.out.println(this); //toString test
+		
+		System.out.println("\nTesting random map:");
+		int[][] newMap = randomMap(1, 30);
+		for (int i = 0; i < newMap.length; i++) {
+			System.out.print(i + 1 + ": ");
+			for (int j = 0; j < newMap[0].length; j++) {
+				System.out.print(newMap[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 	
 	//-------------------------------------------------------------------Adjacent Rooms Methods-------------------------------------------------------------------//
 	
 	//Returns an array of the rooms which are adjacent to a select room (restricted to 1 - 30)
-	public int[] adjacentRooms(int room) {
+	
+	//-------------------------------------------------------------------Adjacent Rooms Methods-------------------------------------------------------------------//
+	
+	//Returns an array of the rooms which are adjacent to a select room (restricted to 1 - 30)
+ 	public int[] adjacentRooms(int room) {
 		int[] rooms = new int[6];
 		
 		//Get room indexes in 2d array
@@ -131,16 +251,16 @@ public class Cave {
 		
 		//Finding room above current room
 		if (roomRow == 0) {
-			rooms[2] = vectorIndexToRoom(caveMap.length - 1, roomCol); //Wrap around to bottom room
+			rooms[2] = vectorIndexToRoom(this.mapRows - 1, roomCol); //Wrap around to bottom room
 		} else {
-			rooms[2] = room - caveMap[0].length;
+			rooms[2] = room - this.mapCols;
 		}
 		
 		//Finding room below current room
-		if (roomRow == caveMap.length - 1) {
+		if (roomRow == this.mapRows - 1) {
 			rooms[3] = vectorIndexToRoom(0, roomCol); //Wrap around to top room
 		} else {
-			rooms[3] = room + caveMap[0].length;
+			rooms[3] = room + this.mapCols;
 		}
 		
 		//Determine whether the last two adjacent rooms are above or below
@@ -158,21 +278,20 @@ public class Cave {
 		return rooms;
 	}
 	
-	
 	//Returns the numbers of the rooms to the immediate left and right of a select room (1 - 30)
 	//roomRow and roomCol are the room's indexes in the 2d array
-	private int[] adjacentRoomHelper(int room, int roomRow, int roomCol) {
+ 	private int[] adjacentRoomHelper(int room, int roomRow, int roomCol) {
 		int[] rooms = new int[2];
 		
 		//Finds room to the immediate left
 		if (roomCol == 0) {
-			rooms[0] = vectorIndexToRoom(roomRow, caveMap[0].length - 1); //wrap around to last room
+			rooms[0] = vectorIndexToRoom(roomRow, this.mapCols - 1); //wrap around to last room
 		} else {
 			rooms[0] = room - 1;
 		}
 		
 		//Finds room to the immediate right
-		if (roomCol == caveMap[0].length - 1) {
+		if (roomCol == this.mapCols - 1) {
 			rooms[1] = vectorIndexToRoom(roomRow, 0); //wrap around to first room
 		} else {
 			rooms[1] = room + 1;
@@ -180,22 +299,19 @@ public class Cave {
 		return rooms;
 	}
 	
-	
 	//Returns the row number of a room (restricted to 1 - 30)
-	private int roomRow(int room) {
-		int row = room / caveMap[0].length;
-		return (room % caveMap[0].length == 0) ? row - 1 : row; //all the multiples of the row length must be reduced by one
+ 	private int roomRow(int room) {
+		int row = room / this.mapCols;
+		return (room % this.mapCols == 0) ? row - 1 : row; //all the multiples of the row length must be reduced by one
 	}
-	
 	
 	//Returns the column number of a room (restricted to 1 - 30)
-	private int roomCol(int room) {
-		return room - (roomRow(room) * caveMap[0].length) - 1;	//Converts room to a number from the first row, then subtracts 1
+ 	private int roomCol(int room) {
+		return room - (roomRow(room) * this.mapCols) - 1;	//Converts room to a number from the first row, then subtracts 1
 	}
 	
-	
 	//Returns the room number given a 2d index
-	private int vectorIndexToRoom(int row, int column) {
-		return row * caveMap[0].length + column + 1; //converts to 1d index and adds one for room number
+ 	private int vectorIndexToRoom(int row, int column) {
+		return row * this.mapCols + column + 1; //converts to 1d index and adds one for room number
 	}
 }
--- Hunt the Wumpus/src/Room.java
+++ Hunt the Wumpus/src/Room.java
@@ -7,18 +7,20 @@
  * 3/26/2019 - Added functionality to allow each room to point to three adjacent rooms
  * 3/27/2019 - Testing bitbucket -> discord functionality (just this line)
  * 4/17/2019 - Added a hazard property
+ * 4/24/2019 - Updated getters and setters for hazards
  */
 
 //NOTE: For connected rooms, the int can be either 0 or 1 - 30: if 0, that pathway isn't used 
 
 public class Room {
 	private int[] connectedRooms = new int[3];	//Allow a room to point to a maximum of 3 others, use 0 to indicate an unused pointer
-	private int hazardType = 0; 				//Determine what hazard a room has, 0 = none, 1 = pit, 2 = bat
+	private int hazardType; 					//Determine what hazard a room has, 0 = none, 1 = pit, 2 = bat
 	
 	public Room(int r1, int r2, int r3) {
 		connectedRooms[0] = r1;
 		connectedRooms[1] = r2;
 		connectedRooms[2] = r3;
+		hazardType = 0;
 	}
 	
 	//Returns an array containing connected rooms to a room
@@ -26,6 +28,12 @@ public class Room {
 		return connectedRooms;
 	}
 	
+	//Sets the hazard type, checks to make sure it is between 0 and 2
+	public void setHazard(int hazardType) {
+		if (hazardType >= 0 && hazardType <= 2)
+			this.hazardType = hazardType;
+	}
+		
 	//Returns the type of hazard the room has, or 0 if none
 	public int getHazard() {
 		return hazardType;
